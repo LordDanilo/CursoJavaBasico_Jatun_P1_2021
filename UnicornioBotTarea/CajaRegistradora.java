89e9546c7bcbec280a6928e9cad00381a89788d9
LordDanilo <64174678+LordDanilo@users.noreply.github.com>
2021-09-25 12:55:13 -0400

    Primeros Ejercicios

---
+++ UnicornioBotTarea/CajaRegistradora.java
@@ -0,0 +1,16 @@
+public class CajaRegistradora{
+    private int total;
+    
+    public CajaRegistradora(){
+        
+    }
+    public String registrarPrecio(int precio, int total){
+        
+    }
+    public int consultarTotal(){
+        
+    }
+    public String cobrar(int dinero){
+        
+    }
+}
